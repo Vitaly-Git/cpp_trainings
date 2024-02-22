77163528910ed74ea51aecae60f831b0b0eb3290
Vitaly <vd.lvov@gmail.com>
2024-02-23 02:02:56 +0300

    multiply files

--- main.cpp
+++ main.cpp
@@ -3,92 +3,10 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-
-void printArray();
-void printNumbersAsText();
-std::string int2text(int num);
+#include "printArray.hpp"
 
 int main(){
-    
     printArray();
     //printNumbersAsText();
     return 0;
-}
-
-void printArray(){
-
-    int arraySize = 0;
-
-    std::cin >> arraySize;
-    int array[arraySize];
-
-    for (int i=0; i<arraySize; ++i)
-        std::cin >> array[i];
-
-    for (int i=arraySize-1; i>=0; --i){
-        std::cout << array[i]; 
-        if (i>0)
-            std::cout << " "; 
-    }
-}
-
-void printNumbersAsText(){
-    
-    std::string numSrtA;
-    std::string numSrtB;
-    int numA = 0;
-    int numB = 0;
-
-    std::cin >> numSrtA;
-    std::cin >> numSrtB;
-
-    numA = std::stoi(numSrtA);
-    numB = std::stoi(numSrtB);
-
-    for(int i=numA; i <= numB; i++)
-        std::cout << int2text(i) << std::endl;
-
-}
-
-std::string int2text(int num){
-
-    std::string retStr = "";
-
-    switch (num){
-        case 0:
-            retStr = "zero";
-            break;
-        case 1:
-            retStr = "one";
-            break;
-        case 2:
-            retStr = "two";
-            break;
-        case 3:
-            retStr = "three";
-            break;
-        case 4:
-            retStr = "four";
-            break;
-        case 5:
-            retStr = "five";
-            break;
-        case 6:
-            retStr = "six";
-            break;
-        case 7:
-            retStr = "seven";
-            break;
-        case 8:
-            retStr = "eight";
-            break;
-        case 9:
-            retStr = "nine";
-            break;
-        default:
-            retStr = num%2 == 0 ? "even" : "odd";
-            break;
-    }
-
-    return retStr;
 }
---
+++ printArray.hpp
@@ -0,0 +1,9 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+void printArray();
+void printNumbersAsText();
+std::string int2text(int num);
