f1517fb70e12d35287d2355bfe83dbf0667b4e37
Vitaly <vd.lvov@gmail.com>
2024-03-09 04:44:45 +0300

    rope

--- ya_alg/7. ya_alg_rope/rope.cpp
+++ ya_alg/7. ya_alg_rope/rope.cpp
@@ -3,20 +3,33 @@
 #include <cassert>
 #include <iterator>
 
-int32_t rope(int32_t n, std::vector<int32_t> ropesLengths);
+int32_t rope(const std::vector<int32_t>& ropesLengths);
 void ropeTest();
 
 int main(){
-    ropeTest();
+    // ropeTest();
+
+    int32_t n;
+    std::cin >> n;
+    std::vector<int32_t> ropeLengths(n); 
+
+    for(int32_t i=0; i<n; ++i)
+        std::cin >> ropeLengths[i];
+
+    std::cout << rope(ropeLengths);
+
     return 0;
 }
 
 void ropeTest(){
-    assert(rope(4, {1,5,2,1}) == 1);
-    assert(rope(4, {5,12,4,3}) == 24);
+    assert(rope({1,5,2,1}) == 1);
+    assert(rope({5,12,4,3}) == 24);
+    assert(rope({3,4,5}) == 12);
+
+    std::cout << "OK ropeTest";
 }
 
-int32_t rope(int32_t n, std::vector<int32_t> ropesLengths){
+int32_t rope(const std::vector<int32_t>& ropesLengths){
 
     int32_t minRopeLen = 0;
 
@@ -26,8 +39,14 @@ int32_t rope(int32_t n, std::vector<int32_t> ropesLengths){
         maxRope = std::max(maxRope, *it);
         sumRope += *it;
     }
+    sumRope -= maxRope;
 
-    
+    if (sumRope < maxRope)
+        minRopeLen = maxRope - sumRope;
+    else if (sumRope > maxRope)
+        minRopeLen = maxRope + sumRope;
+    else    
+        minRopeLen = maxRope*2;
 
     return minRopeLen;
 }
