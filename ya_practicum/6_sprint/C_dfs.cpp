af6dd9f496be69cbf5d1c1b4f0a1d40060137bd9
Vitaly <vd.lvov@gmail.com>
2024-05-08 23:57:18 +0300

    s6 c_dfs

--- ya_practicum/6_sprint/C_dfs.cpp
+++ ya_practicum/6_sprint/C_dfs.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
+#include <algorithm>
 
 struct Vertexes{
     int vertexCount = 0;
@@ -12,6 +13,7 @@ typedef std::vector<Vertexes> VertexesVector;
 
 void startDfs(VertexesVector& vertexesVector, Colors& color);
 void Dfs(VertexesVector& vertexesVector, Colors& color, int v);
+void startDfsFromVertex(VertexesVector& vertexesVector, int startVertex, Colors& color);
 
 int main(){
 
@@ -26,14 +28,23 @@ int main(){
         std::cin >> u >> v;
         ++(vertexesVector[u].vertexCount);
         vertexesVector[u].vertexes.push_back(v);
+        // Граф не ориентированный, поэтому добавим два вектора
+        ++(vertexesVector[v].vertexCount);
+        vertexesVector[v].vertexes.push_back(u);        
     }
 
-    Colors color(n);
+    for (int idxVertex = 0; idxVertex<=n; ++idxVertex){
+        std::sort(vertexesVector[idxVertex].vertexes.begin(), vertexesVector[idxVertex].vertexes.end());        
+    }   
+
+    int startVectice;
+    std::cin >> startVectice;
+
+    Colors color(n+1);
     for(auto it = color.begin(); it!=color.end(); ++it)
         *it = "white";
 
-    startDfs(vertexesVector, color);
-
+    startDfsFromVertex(vertexesVector, startVectice, color);
 
     // for (int i=1; i<n+1; ++i){
     //     std::cout << result[i].vertexCount;
@@ -43,6 +54,14 @@ int main(){
     // }
 
     return 0;
+}
+
+void startDfsFromVertex(VertexesVector& vertexesVector, int startVertex, Colors& color){
+    // for (int i = 0; i < color.size(); ++i) {
+    //     // Перебираем варианты стартовых вершин, пока они существуют.
+    //     if (color[i] == "white")
+            Dfs(vertexesVector, color, startVertex); // Запускаем обход, стартуя с i-й вершины.
+    // }
 }
 
 void startDfs(VertexesVector& vertexesVector, Colors& color){
@@ -57,8 +76,10 @@ void Dfs(VertexesVector& vertexesVector, Colors& color, int v) { // v - номе
     color[v] = "gray"; // Вершина посещена, но ещё не обработана.
     //for (int w : outgoing_edges(v)) { // Получите список исходящих ребер в зависимости от способа хранения графа
     for (int w : vertexesVector[v].vertexes) { // Получите список исходящих ребер в зависимости от способа хранения графа
-        if (color[w] == "white") // Если вершина не посещена, то
+        if (color[w] == "white"){ // Если вершина не посещена, то
             Dfs(vertexesVector, color, w); // запустим обход от найденной смежной вершины.
+            std::cout << w << " ";
+        }
     }
     color[v] = "black"; // Теперь вершина обработана.
     std::cout << v;
