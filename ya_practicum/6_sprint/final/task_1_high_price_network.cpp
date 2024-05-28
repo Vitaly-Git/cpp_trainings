16ba291a936f04d36805993ed658619465869ab2
Vitaly <vd.lvov@gmail.com>
2024-05-29 01:40:51 +0300

    s6 final A ready

--- ya_practicum/6_sprint/final/task_1_high_price_network.cpp
+++ ya_practicum/6_sprint/final/task_1_high_price_network.cpp
@@ -7,14 +7,23 @@
 #include <vector>
 #include <algorithm>
 
-std::map<int, int> mapVertexSet;
-
 struct Edge{
   int vertice1 = 0;
   int vertice2 = 0;
   int weight = 0;
 };
-std::vector<Edge> edges; 
+struct Vertices{
+    int verticeCount = 0;
+    std::vector<int> vertexes;
+};
+
+typedef std::vector<int> Colors;
+typedef std::vector<Vertices> VerticesVector;
+
+int maxOstTree(int verticeCount, std::vector<Edge>& edges);
+int getConnectedComponentCount(VerticesVector& verticesVector, int n);
+void startDfsToCalcConnectedComponentCount(VerticesVector& verticesVector, Colors& color, int& connectedComponentCount);
+void Dfs(VerticesVector& verticesVector, Colors& color, int v, int& connectedComponentCount);
 
 int main(){
 
@@ -22,103 +31,108 @@ int main(){
   int m=0;
   std::cin >> n >> m;
 
-  edges.resize(n);
+  std::vector<Edge> edges(m); 
+  VerticesVector verticesVector(n+1);
 
   int u,v,w;
   for (int idxEdge = 0; idxEdge<m; ++idxEdge){
-    std::cin >> edges[idxEdge].vertice1 >> edges[idxEdge].vertice2 >> edges[idxEdge].weight;
-    edges[idxEdge].weight = -edges[idxEdge].weight;
+    std::cin >> u >> v >> w;
+
+    edges[idxEdge].vertice1 = --u;
+    edges[idxEdge].vertice2 = --v;
+    edges[idxEdge].weight = -w;
+
+    ++(verticesVector[u].verticeCount);
+    verticesVector[u].vertexes.push_back(v);
+    ++(verticesVector[v].verticeCount);
+    verticesVector[v].vertexes.push_back(u); 
   }
 
+  int connectedComponentCount = getConnectedComponentCount(verticesVector, n);
 
+  int maxOst = maxOstTree(n, edges);
+  if ((maxOst == 0 || connectedComponentCount > 1) && !(n==1 && m==0))
+    std::cout << "Oops! I did it again" << '\n';
+  else
+    std::cout << maxOst << '\n';
 
-
-
-    return 0;
+  return 0;
 }
 
-minimum_spanning_tree = []   # Рёбра, составляющие MST.
+int maxOstTree(int verticeCount, std::vector<Edge>& edges){
+  int maxOst = 0;
 
-added = {}          # Множество вершин, уже добавленных в остов.
-not_added = {}      # Множество вершины, ещё не добавленных в остов. 
-edges = []          # Массив рёбер, исходящих из остовного дерева.
+  std::map<int, int> mapVertexSet;
+  std::vector<std::vector<int>> setsItems(verticeCount);
 
-функция add_vertex(v)
-  added += v
-  not_added -= v
-  # Добавляем все рёбра, которые инцидентны v, но их конец ещё не в остове.
-  #
-  # Вершины not_added стоит хранить в таком контейнере,
-  # чтобы проверка (end in not_added) выполнялась эффективно.
-  #
-  # Для этого подойдёт, например, хеш-таблица.
-  # Также в некоторых языках программирования имеется контейнер set.
-  edges += graph.edges.filter(start == v, end in not_added)
-
-
-функция find_MST(graph):
-  not_added = graph.vertices
+  for(int itemIdx=0; itemIdx < verticeCount; ++itemIdx){
+    mapVertexSet[itemIdx] = itemIdx;
     
-  # Берём первую попавшуюся вершину.
-  v = graph.vertices[0]
-  add_vertex(v)
+    std::vector<int>* itemsOfSet = new std::vector<int>{itemIdx};
+    setsItems[itemIdx] = *itemsOfSet;
+  }
 
-  пока not_added не пуст и edges не пуст:
-    # Подразумеваем, что extract_minimum извлекает минимальное ребро 
-    # из массива рёбер и больше данного ребра в массива не будет.
-    e = extract_minimum(edges)
-    если e.end in not_added, то:
-      minimum_spanning_tree += e
-      add_vertex(e.end)
+  std::sort(edges.begin(), edges.end(), [](auto& lhs, auto& rhs)
+    {
+      if (lhs.weight!=rhs.weight)
+        return lhs.weight<rhs.weight;
+      
+      //return true;
 
-  если not_added не пуст, то 
-    верни ошибку "Исходный граф несвязный"
-  иначе
-    верни minimum_spanning_tree
+      if (lhs.vertice1!=rhs.vertice1)
+         return lhs.vertice1<rhs.vertice1;
 
+      return lhs.vertice2<rhs.vertice2;
+    });
 
+  for (auto& edge: edges){
+    if (mapVertexSet[edge.vertice1] == mapVertexSet[edge.vertice2])
+      continue;
 
+    int oldSetIdx = mapVertexSet[edge.vertice2];
+    int newSetIdx = mapVertexSet[edge.vertice1];
 
+    for (auto item : setsItems[oldSetIdx]){
+      setsItems[newSetIdx].push_back(item);
+      mapVertexSet[item] = newSetIdx;
+    }
+    setsItems[oldSetIdx].clear();
+    
+    maxOst += edge.weight;
+  }
 
-var minimumSpanningTree = mutableListOf<Edge>()  // Рёбра, составляющие MST.
-
-val added = mutableSetOf<Vertex>()  // Множество вершин, уже добавленных в остов.
-val notAdded = mutableSetOf<Vertex>()  // Множество вершин, ещё не добавленных в остов.
-val edges = mutableListOf<Edge>()  // Массив рёбер, исходящих из остовного дерева.
-
-fun addVertex(v: Vertex) {
-    added += v
-    notAdded -= v
-    // Добавляем все рёбра, которые инцидентны v, но их конец ещё не в остове.
-    //
-    // Вершины notAdded стоит хранить в таком контейнере,
-    // чтобы проверка (end in notAdded) выполнялась эффективно.
-    //
-    // Для этого подойдёт, например, хеш-таблица.
-    // Также в некоторых языках программирования имеется контейнер set.
-    edges += graph.edges.filter { it.start == v && it.end in notAdded }
+  return -maxOst;
 }
 
-fun findMST(graph: Graph): List<Edge> {
-    notAdded.addAll(graph.vertices)
+int getConnectedComponentCount(VerticesVector& verticesVector, int n){
+  int connectedComponentCount = 1;
 
-    // Берём первую попавшуюся вершину.
-    val v = graph.vertices[0]
-    addVertex(v)
+  for (int idxVertex = 0; idxVertex<=n; ++idxVertex)
+      std::sort(verticesVector[idxVertex].vertexes.begin(), verticesVector[idxVertex].vertexes.end());        
+  
+  Colors color(n+1);
+  for(auto it = color.begin(); it!=color.end(); ++it)
+      *it = -1;
 
-    while (notAdded.isNotEmpty() && edges.isNotEmpty()) {
-        // Подразумеваем, что extractMinimum извлекает минимальное ребро
-        // из массива рёбер и больше данного ребра в массива не будет.
-        val e = extractMinimum(edges)
-        if (e.end in notAdded) {
-            minimumSpanningTree += e
-            addVertex(e.end)
+  startDfsToCalcConnectedComponentCount(verticesVector, color, connectedComponentCount);
+  return connectedComponentCount - 2;
+}
+
+void startDfsToCalcConnectedComponentCount(VerticesVector& verticesVector, Colors& color, int& connectedComponentCount){
+    for (int i = 0; i < color.size(); ++i) {
+        if (color[i] == -1){
+            Dfs(verticesVector, color, i, connectedComponentCount);
+            ++connectedComponentCount;
         }
     }
+}
 
-    return if (notAdded.isNotEmpty()) {
-        error("Исходный граф несвязный")
-    } else {
-        minimumSpanningTree
+void Dfs(VerticesVector& verticesVector, Colors& color, int v, int& connectedComponentCount) {
+    color[v] = 0;
+    for (int w : verticesVector[v].vertexes) {
+        if (color[w] == -1){
+            Dfs(verticesVector, color, w, connectedComponentCount);
+        }
     }
-}    
+    color[v] = connectedComponentCount;
+}
