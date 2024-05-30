28b265e0eabd830d3199b8a45826ccc124d1f2cd
Vitaly <vd.lvov@gmail.com>
2024-05-31 01:49:55 +0300

    s6 final task ready

--- ya_practicum/6_sprint/final/task_2_railways.cpp
+++ ya_practicum/6_sprint/final/task_2_railways.cpp
@@ -3,85 +3,90 @@
 #include <iterator>
 #include <algorithm>
 
-struct Vertexes{
-    int vertexCount = 0;
-    std::vector<int> vertexes;
+struct Vertices{
+    int VerticeCount = 0;
+    std::vector<int> Vertices;
 };
 
 typedef std::vector<std::string> Colors;
-typedef std::vector<Vertexes> VertexesVector;
+typedef std::vector<Vertices> VerticesVector;
 
-void startDfs(VertexesVector& vertexesVector, Colors& color);
-void Dfs(VertexesVector& vertexesVector, Colors& color, int v);
-void startDfsFromVertex(VertexesVector& vertexesVector, int startVertex, Colors& color);
+bool isGraphCycled(VerticesVector& VerticesVector, Colors& color);
+void startDfs(VerticesVector& VerticesVector, Colors& color, bool& graphCycled);
+void Dfs(VerticesVector& VerticesVector, Colors& color, int v, bool& graphCycled);
 
 int main(){
 
-    int n=0;
-    int m=0;
-    std::cin >> n >> m;
+    // Ввод количества городов
 
-    VertexesVector vertexesVector(n+1);
+    int n=0;
+    std::cin >> n;
+
+    VerticesVector VerticesVector(n+1);
+
+    // Превращаем символы в ребра графа:
+    // R - прямое ребро
+    // B - обратное ребро
 
     int u,v;
-    for (int idxEdge = 0; idxEdge<m; ++idxEdge){
-        std::cin >> u >> v;
-        ++(vertexesVector[u].vertexCount);
-        vertexesVector[u].vertexes.push_back(v);
-        // Граф не ориентированный, поэтому добавим два вектора
-        ++(vertexesVector[v].vertexCount);
-        vertexesVector[v].vertexes.push_back(u);        
+    char direction;
+    for (int idxPathPoint1 = 0; idxPathPoint1<n; ++idxPathPoint1){
+        
+        for (int idxPathPoint2 = idxPathPoint1+1; idxPathPoint2<n; ++idxPathPoint2){
+            std::cin >> direction;
+            if (direction == 'R'){
+                u = idxPathPoint1;
+                v = idxPathPoint2;
+            } else {
+                u = idxPathPoint2;
+                v = idxPathPoint1;
+            }
+            ++(VerticesVector[u].VerticeCount);
+            VerticesVector[u].Vertices.push_back(v);                    
+        }
     }
 
-    for (int idxVertex = 0; idxVertex<=n; ++idxVertex){
-        std::sort(vertexesVector[idxVertex].vertexes.begin(), vertexesVector[idxVertex].vertexes.end());        
-    }   
-
-    int startVectice;
-    std::cin >> startVectice;
+    for (int idxVertice = 0; idxVertice<=n; ++idxVertice)
+        std::sort(VerticesVector[idxVertice].Vertices.begin(), VerticesVector[idxVertice].Vertices.end());        
 
     Colors color(n+1);
     for(auto it = color.begin(); it!=color.end(); ++it)
         *it = "white";
 
-    startDfsFromVertex(vertexesVector, startVectice, color);
+    // Обходим граф через DFS, выполняя поиск цикло в графе
 
-    // for (int i=1; i<n+1; ++i){
-    //     std::cout << result[i].vertexCount;
-    //     for (int j=0; j<result[i].vertexes.size(); ++j)
-    //         std::cout << " " << result[i].vertexes[j];
-    //     std::cout << "\n";
-    // }
+    if (isGraphCycled(VerticesVector, color))
+        std::cout << "NO";
+    else    
+        std::cout << "YES";
 
     return 0;
 }
 
-void startDfsFromVertex(VertexesVector& vertexesVector, int startVertex, Colors& color){
-    // for (int i = 0; i < color.size(); ++i) {
-    //     // Перебираем варианты стартовых вершин, пока они существуют.
-    //     if (color[i] == "white")
-        std::cout << startVertex << " ";
-            Dfs(vertexesVector, color, startVertex); // Запускаем обход, стартуя с i-й вершины.
-    // }
+bool isGraphCycled(VerticesVector& VerticesVector, Colors& color){
+    bool graphCycled = false;
+    startDfs(VerticesVector, color, graphCycled);
+    return graphCycled; 
 }
 
-void startDfs(VertexesVector& vertexesVector, Colors& color){
+void startDfs(VerticesVector& VerticesVector, Colors& color, bool& graphCycled){
     for (int i = 0; i < color.size(); ++i) {
-        // Перебираем варианты стартовых вершин, пока они существуют.
         if (color[i] == "white")
-            Dfs(vertexesVector, color, i); // Запускаем обход, стартуя с i-й вершины.
+            Dfs(VerticesVector, color, i, graphCycled);
+            if (graphCycled)
+                return;
     }
 }
 
-void Dfs(VertexesVector& vertexesVector, Colors& color, int v) { // v - номер вершины
-    color[v] = "gray"; // Вершина посещена, но ещё не обработана.
-    //for (int w : outgoing_edges(v)) { // Получите список исходящих ребер в зависимости от способа хранения графа
-    for (int w : vertexesVector[v].vertexes) { // Получите список исходящих ребер в зависимости от способа хранения графа
-        if (color[w] == "white"){ // Если вершина не посещена, то
-            std::cout << w << " ";
-            Dfs(vertexesVector, color, w); // запустим обход от найденной смежной вершины.
+void Dfs(VerticesVector& VerticesVector, Colors& color, int v, bool& graphCycled) {
+    color[v] = "gray";
+    for (int w : VerticesVector[v].Vertices) {
+        if (color[w] == "white"){
+            Dfs(VerticesVector, color, w, graphCycled);
+        }else if (color[w] == "gray") {
+            graphCycled = true;
+            return;
         }
     }
-    color[v] = "black"; // Теперь вершина обработана.
-    // std::cout << v;
+    color[v] = "black";
 }
