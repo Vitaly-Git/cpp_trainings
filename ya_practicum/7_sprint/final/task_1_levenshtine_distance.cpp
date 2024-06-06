f6d37564a9df0b1743f906ff0cc78affdc655059
Vitaly <vd.lvov@gmail.com>
2024-06-06 23:57:08 +0300

    s7 final

--- ya_practicum/7_sprint/final/task_1_levenshtine_distance.cpp
+++ ya_practicum/7_sprint/final/task_1_levenshtine_distance.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 int main(){
+
+    // std::vector<char> word1;
+    // std::vector<char> word2;
+
+    std::string word1;
+    std::string word2;
+
+    std::getline(std::cin, word1);
+    std::getline(std::cin, word2);
+
+    std::vector< std::vector<int> > rowsByWord1(word1.size());
+
+    for (int i=0; i<word1.size(); ++i)
+        rowsByWord1[0] = std::vector<int>(word2.size());
+
+    for (int y=0; y<word1.size(); ++y){
+        for (int x=0; x<word2.size(); ++x){
+            
+        }
+    }
 
 
     return 0;
