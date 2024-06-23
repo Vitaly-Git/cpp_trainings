c8754dba55b0c3ace9c1604c61172abf428643af
Vitaly <vd.lvov@gmail.com>
2024-06-23 10:55:56 +0300

    async ops shared ptr lambda

--- ya_pr_cpp_back/ya_pr_cpp_back_lessons_task/breakfast/src/main.cpp
+++ ya_pr_cpp_back/ya_pr_cpp_back_lessons_task/breakfast/src/main.cpp
@@ -10,7 +10,9 @@
 #include <iostream>
 #include <thread>
 
-//#define SYNC
+// #define SYNC
+// #define ASYNC
+#define ASYNC_SMART
 
 namespace net = boost::asio;
 namespace sys = boost::system;
@@ -19,13 +21,20 @@ using namespace std::literals;
 
 void syncBreakfast();
 void asyncBreakfast();
+void asyncSmartBreakfast();
 
 int main() {
 
   #ifdef SYNC
     syncBreakfast();
-  #else
+  #endif
+
+  #ifdef ASYNC
     asyncBreakfast();
+  #endif
+
+  #ifdef ASYNC_SMART
+    asyncSmartBreakfast();
   #endif
 
 }
@@ -64,6 +73,7 @@ void syncBreakfast() {
 }
 
 void asyncBreakfast(){
+
 
    // Контекст для выполнения операций ввода-вывода.
    // Другие классы библиотеки выполняют с его помощью операции ввода-вывода
@@ -107,5 +117,56 @@ void asyncBreakfast(){
         std::cout << "Enjoy your meal"sv << std::endl;
     } catch (const std::exception& e) {
         std::cout << e.what() << std::endl;
+    } 
+}
+
+void asyncSmartBreakfast(){
+
+   // Контекст для выполнения операций ввода-вывода.
+   // Другие классы библиотеки выполняют с его помощью операции ввода-вывода
+   net::io_context io;
+
+   // Замеряем время начала программы
+   const auto start_time = steady_clock::now();
+
+    // Жарим яичницу 3 секунды
+    {
+        auto t = std::make_shared<net::steady_timer>(io, 3s);
+        std::cout << "Fry eggs"sv << std::endl;
+        // Захват переменной t внутри лямбда-функции продлит время жизни таймера
+        // до тех пор, пока не будет вызван обработчик.
+        t->async_wait([t](sys::error_code ec) {
+            if (ec) {
+                throw std::runtime_error("Wait error: "s + ec.message());
+            }
+            std::cout << "Put eggs onto the plate. Thread id: "sv << std::this_thread::get_id()
+                      << std::endl;
+        });
     }
+
+    // Варим кофе 5 секунд
+    {
+        auto t = std::make_shared<net::steady_timer>(io, 5s);
+        std::cout << "Brew coffee"sv << std::endl;
+        t->async_wait([t](sys::error_code ec) {
+            if (ec) {
+                throw std::runtime_error("Wait error: "s + ec.message());
+            }
+            std::cout << "Pour coffee in the cup. Thread id: "sv << std::this_thread::get_id()
+                      << std::endl;
+        });
+    }
+
+    // Запускаем обработку асинхронных операций
+    try {
+        std::cout << "Run asynchronous operations"sv << std::endl;
+        io.run();
+        const auto cook_duration = duration<double>(steady_clock::now() - start_time);
+        std::cout << "Breakfast has been cooked in "sv << cook_duration.count() << "s"sv << std::endl;
+        std::cout << "Thread id: "sv << std::this_thread::get_id() << std::endl;
+        std::cout << "Enjoy your meal"sv << std::endl;
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+
 }
