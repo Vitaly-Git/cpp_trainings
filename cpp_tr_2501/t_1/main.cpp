2a8a2c01c3fd9922e6c5b987618fd03b2f6ba0e6
Vitaly <vd.lvov@gmail.com>
2025-02-21 01:23:39 +0300

    commit -m "sun"

--- cpp_tr_2501/t_1/main.cpp
+++ cpp_tr_2501/t_1/main.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
 
-class WorldObject{
+
+class Sun{
+
+private:
+    Sun(){};
+    static Sun* sun_;
+
+public:
+    Sun(Sun &other) = delete;
+    void operator=(const Sun) = delete;
+    
+    static Sun* GetSun(){
+        if (sun_ == nullptr)
+            sun_ = new Sun();
+        return sun_;    
+    };
+};
+
+Sun* Sun::sun_ = nullptr;
+
+class SimpleObject{
 public:    
     std::string name;
     int64_t weight;
     std::string to_string();
 };
 
-std::string WorldObject::to_string(){
+std::string SimpleObject::to_string(){
     return "Name: " + name + "\n" + "Weight: " + std::to_string(weight) + "\n";
 }
 
 int main(){
-    WorldObject stoneOne;
+    std::cout << "Sun " << Sun::GetSun() << "\n"; 
+
+    SimpleObject stoneOne;
 
     stoneOne.name = "FirstStoneAgain"; 
     stoneOne.weight = 0;
 
     std::cout << "Hello" << "\n";
     std::cout << stoneOne.to_string() << "\n";
+
+    std::cout << "Sun " << Sun::GetSun() << "\n";     
 }
