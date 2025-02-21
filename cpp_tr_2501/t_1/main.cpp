836e01d516c439690a3143dff2c34ef723518c4e
Vitaly <vd.lvov@gmail.com>
2025-02-21 23:53:26 +0300

    files

--- cpp_tr_2501/t_1/main.cpp
+++ cpp_tr_2501/t_1/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <fstream>
 
 class Sun{
 
@@ -31,6 +31,8 @@ std::string SimpleObject::to_string(){
     return "Name: " + name + "\n" + "Weight: " + std::to_string(weight) + "\n";
 }
 
+int FileWork();
+
 int main(){
     std::cout << "Sun " << Sun::GetSun() << "\n"; 
 
@@ -43,4 +45,26 @@ int main(){
     std::cout << stoneOne.to_string() << "\n";
 
     std::cout << "Sun " << Sun::GetSun() << "\n";     
+
+    FileWork();
+}
+
+int FileWork(){
+    std::ofstream fileToWrite;
+    std::ifstream fileToRead;
+    
+    std::string fileName = "test_file_2.txt";
+    std::string textFromFile;
+
+    fileToWrite.open(fileName);
+    fileToWrite << "test string";
+    fileToWrite.close();
+
+    fileToRead.open(fileName);
+    std::getline(fileToRead, textFromFile);
+    
+
+    //fileToRead >> textFromFile;
+
+    std::cout << textFromFile << std::endl;
 }
